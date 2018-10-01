543956e1382e9629a69466c3029bfdb13e0fc516
psr <prabhjotsinghrai1@gmail.com>
2018-10-02 04:32:55 +0530

    feat: change pdf to desired format

--- random/test.py
+++ random/test.py
@@ -15,7 +15,8 @@ def min_cost(i, j, cost_matrix):
     # been computed
     for m in range(1, i + 1):
         for n in range(1, j + 1):
-            cost_matrix[m][n] = min(cost_matrix[m - 1][n], cost_matrix[m][n - 1]) \
+            cost_matrix[m][n] = min(cost_matrix[m - 1][n],
+                                    cost_matrix[m][n - 1]) \
                                       + cost_matrix[m][n]
 
     return cost_matrix[i][j]
@@ -77,6 +78,6 @@ def longest_pallindrome(input_string):
     cache[input_string] = longest_length
     return longest_length
 
-print(longest_pallindrome('animal'))
-
+# print(longest_pallindrome('animal'))
+print(min_cost(1,2, [[1,1,1],[1,1,3]]))
 # print(longestPalSubstr('d a b b d d e a c a b c a a d c b c e a e d b a e b a a e d e d c e b c e d e c d a b b a b a b d b b b b e d d a d a b e d c d e d b a b b b e e b d b e c b b c b c e b b e c a a a d d c a b e c e e a d d c b d a c c e e b c a e e b d d a d c d c a b a e e e b a e e a c a d d a c c b e c b c d b b e d c c a b b c b b a d b e e a b d b a e c c d e a a c c c d d e a c b e b a c c b b e c e c a e e e e e a c b c a b e b d c c e e c d a b e c c d a c c e a e a a c d b a e b c d c c c e b d b b d b b d a c e e a b b e e e b a e d d e d a e b d e a a a c b d d b d b c e e a c b a d d b d c c a a e e c e c a a b e d e e b b a d e c d a e e e e e a d b e e e e b d a e e b e c b d b a a b e e b d b e c e a e a e b a c d b d c b c b d a e c d d d b a d b b e d b b c c b a c e d e d b c a b c c d c'))
