b213ae6e4ca527fc87302749586cf7697d51a3b1
psr <prabhjotsinghrai1@gmail.com>
2018-10-12 23:12:45 +0530

    chore: changes to reconstruct function

--- homeworks/reconstruct/submission.py
+++ homeworks/reconstruct/submission.py
@@ -1,4 +1,4 @@
-import shell
+# import shell
 import util
 import wordsegUtil
 
@@ -12,23 +12,24 @@ class SegmentationProblem(util.SearchProblem):
 
     def startState(self):
         # BEGIN_YOUR_CODE (our solution is 3 lines of code, but don't worry if you deviate from this)
-        return 0, ''
+        return ''
         # END_YOUR_CODE
 
     def isEnd(self, state):
         # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
-        return state[0] == len(self.query)
+        return len(state.replace(' ', '')) == len(self.query)
         # END_YOUR_CODE
 
     def succAndCost(self, state):
         # BEGIN_YOUR_CODE (our solution is 12 lines of code, but don't worry if you deviate from this)
         paths = []
-        next_state = state[0] + 1
-        while next_state < len(self.query) + 1:
-            built_string = state[1] + self.query[state[0]: next_state]
-            built_string += ' ' if next_state != len(self.query) else ''
-            paths.append((built_string, (next_state, built_string), self.unigramCost(self.query[state[0]: next_state])))
-            next_state += 1
+        current_node = 0 if state == self.startState() else len(state.replace(' ', ''))
+        next_node = current_node + 1
+        while next_node <= len(self.query):
+            built_string = state + self.query[current_node: next_node]
+            built_string += ' ' if next_node != len(self.query) else ''
+            paths.append((built_string, built_string, self.unigramCost(self.query[current_node: next_node])))
+            next_node += 1
         return paths
         # END_YOUR_CODE
 
@@ -40,9 +41,18 @@ def segmentWords(query, unigramCost):
     ucs.solve(SegmentationProblem(query, unigramCost))
 
     # BEGIN_YOUR_CODE (our solution is 10 lines of code, but don't worry if you deviate from this)
-    return ucs.actions[-1] if ucs.actions and len(ucs.actions) > 0 else ''
+    return ucs.actions[-1] if len(ucs.actions) > 0 else ''
     # END_YOUR_CODE
-
+#
+# def unigramCost(x):
+#     if x in ['and', 'two', 'three', 'word', 'words']:
+#         return 1.0
+#     else:
+#         return 1000.0
+#
+# # a = SegmentationProblem('andthreewords', unigramCost)
+# # print a.succAndCost('andthreeword ')
+# print segmentWords('twowords', unigramCost)
 ############################################################
 # Problem 2b: Solve the vowel insertion problem under a bigram cost
 
@@ -106,5 +116,5 @@ def segmentAndInsert(query, bigramCost, possibleFills):
 
 ############################################################
 
-if __name__ == '__main__':
-    shell.main()
+# if __name__ == '__main__':
+#     shell.main()
